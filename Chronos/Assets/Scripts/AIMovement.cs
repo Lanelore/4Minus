0237a338920b63acfee78d9ff5cab0538617616a
Lanelore <bzankl@gmx.at>
2017-12-16 11:11:57 +0100

    Add initial content and calculate spherical gravity and simple ai movement.

---
+++ Chronos/Assets/Scripts/AIMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour {
+    public float walkSpeed = 6; //movement/walking speed
+    Vector3 moveAmount;
+    Vector3 smoothMoveVelocity;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        // set dampig dependend if grounded or not
+        float damping = 1;
+
+        Vector3 moveDir = new Vector3(1, 0, 0).normalized;
+        Vector3 targetMoveAmount = moveDir * walkSpeed;
+
+        moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, 0.15f * damping); //ref allows to modify a global variable
+
+        // Apply movement to rigidbody
+        Vector3 localMove = transform.TransformDirection(moveAmount) * Time.deltaTime; //transform to local space (instead of world space - move on the surface of the sphere)
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + localMove);
+    }
+}
