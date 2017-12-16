037b6f2a1ca748cf92131f473cd88ad81b53570a
Lanelore <bzankl@gmx.at>
2017-12-16 17:14:02 +0100

    Submit scripts.

--- Chronos/Assets/Scripts/AIMovement.cs
+++ Chronos/Assets/Scripts/AIMovement.cs
@@ -6,29 +6,86 @@ public class AIMovement : MonoBehaviour {
     public float walkSpeed = 6; //movement/walking speed
     Vector3 moveAmount;
     Vector3 smoothMoveVelocity;
+    GameObject player;
+    GameObject planet;
+    public GameObject dummy;
+    Vector3 currentTarget;
+    Vector3 currentTargetDirection;
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        GameObject[] planets = GameObject.FindGameObjectsWithTag("Planet");
+        if (planets.Length > 0)
+        {
+            planet = planets[0];
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 
     void FixedUpdate()
     {
-        // set dampig dependend if grounded or not
         float damping = 1;
 
-        Vector3 moveDir = new Vector3(1, 0, 0).normalized;
-        Vector3 targetMoveAmount = moveDir * walkSpeed;
+        Vector3 aiPos = this.transform.position;
+        Vector3 aiPlayerDir = (player.transform.position - this.transform.position).normalized;
+        Vector3 pointTowardsPlayer = aiPos + aiPlayerDir * 4;
+   //     Debug.DrawLine(aiPos, pointTowardsPlayer, Color.red, 1);
 
+        //Raycast experiment
+        RaycastHit hit;
+        Vector3 planetPos = planet.transform.position;
+        // dir = (end - start).normalized
+        Vector3 planetDir = (pointTowardsPlayer - planetPos).normalized;
+        Vector3 outsidePosition = planetPos + planetDir * 50;
+        Vector3 outsideDirection = (planetPos - outsidePosition).normalized;
+        Ray ray = new Ray(outsidePosition, outsideDirection);
+
+        Vector3 pointDirection = (pointTowardsPlayer - aiPos).normalized;
+
+
+       // Debug.DrawLine(outsidePosition, planetPos, Color.blue, Mathf.Infinity);
+      
+
+        //RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            {
+                Debug.Log("Ground");
+                // find target
+                currentTarget = hit.point;
+                // end - start
+                currentTargetDirection = (currentTarget - aiPos).normalized;
+                Debug.Log("Ground");
+                // find target
+                GameObject createdDummy = GameObject.Instantiate(dummy, hit.point, Quaternion.identity) as GameObject;
+                Destroy(createdDummy, 3);
+
+            }
+        }
+
+        //     float mouseSensitivityX = 250;
+        //       transform.Rotate(Vector3.up * pointDirection.y * mouseSensitivityX * Time.deltaTime);
+
+        //  transform.localRotation = Quaternion.Euler(0, currentTargetDirection.y, 0);
+
+        Debug.DrawLine(aiPos, currentTarget, Color.blue, Mathf.Infinity);
+
+
+        Vector3 moveDir = new Vector3(currentTargetDirection.x, currentTargetDirection.y, currentTargetDirection.z).normalized;
+        Vector3 targetMoveAmount = moveDir * walkSpeed;
         moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, 0.15f * damping); //ref allows to modify a global variable
 
-        // Apply movement to rigidbody
+
         Vector3 localMove = transform.TransformDirection(moveAmount) * Time.deltaTime; //transform to local space (instead of world space - move on the surface of the sphere)
         GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + localMove);
+
+        //GetComponent<Rigidbody>().MovePosition(currentTarget);
     }
 }
--- Chronos/Assets/Scripts/GravityBody.cs
+++ Chronos/Assets/Scripts/GravityBody.cs
@@ -9,7 +9,6 @@ public class GravityBody : MonoBehaviour {
 
 	GravityAttractor targetGravity;
 	bool planetGravity = false;
-	GameObject player;
 	GameObject[] planets;
 	GameObject targetPlanet;
 	
@@ -19,32 +18,13 @@ public class GravityBody : MonoBehaviour {
 		planets = GameObject.FindGameObjectsWithTag ("Planet");
 		if (planets.Length > 0) {
 			planetGravity = true;
-			
-			player = GameObject.FindGameObjectWithTag ("Player");
 			targetPlanet = planets [0];
-			//planets = GameObject.FindGameObjectsWithTag("Planet").GetComponent<GravityAttractor>();
-			//planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<GravityAttractor>();
-			
+
 			// Disable rigidbody gravity and rotation as this is simulated in GravityAttractor script
 			GetComponent<Rigidbody> ().useGravity = false;
 			GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezeRotation;
-			
-			InvokeRepeating ("FindTargetPlanet", 2.0f, 0.5f);
 		} else {
-			GetComponent<Rigidbody>().freezeRotation = true;
-		}
-	}
-	
-	void FindTargetPlanet(){
-		float targetDistance = Vector3.Distance (targetPlanet.transform.position, player.transform.position);	
-		for(var i = 0; i < planets.Length; i++)
-		{
-			GameObject tmpPlanet = planets[i];
-			float tmpDistance = Vector3.Distance (tmpPlanet.transform.position, player.transform.position);
-			if(tmpDistance < targetDistance)
-			{
-				targetPlanet = tmpPlanet;
-			}
+			//GetComponent<Rigidbody>().freezeRotation = true;
 		}
 	}
 	
@@ -52,22 +32,14 @@ public class GravityBody : MonoBehaviour {
 	void FixedUpdate () {
 		if (planetGravity) {
 			targetGravity = targetPlanet.GetComponent<GravityAttractor> ();
-			targetGravity.Attract (transform);
-		} else {
-			// Apply downwards gravity to body
-			transform.GetComponent<Rigidbody> ().AddForce (gravityUp * gravity);
-			// Allign bodies up axis with the centre of planet
-			Quaternion targetRotation = Quaternion.FromToRotation (transform.up, gravityUp) * transform.rotation;
-			float angularSpeed = 3.0f;
-			if (transform.rotation != targetRotation) {
-				//transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, angularSpeed);
 
-				transform.rotation = Quaternion.RotateTowards (transform.rotation, targetRotation, angularSpeed);
-					//print("Player: " + transform.rotation + ", target: " + targetRotation);
-					//transform.Rotate(0, yAmount, 0, Space.Self);
-			}
-		}
-		//linear interpolation
-		//Zielposition über Zeitraum
+            if (this.tag == "Player")
+            {
+                targetGravity.Attract(transform);
+            } else
+            {
+                targetGravity.AIAttract(transform);
+            }
+		}	
 	}
 }
