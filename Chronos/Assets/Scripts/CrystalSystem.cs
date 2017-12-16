bc6b90bbcc6f89344c1172a3dce41686a252ebfd
Lanelore <bzankl@gmx.at>
2017-12-16 22:39:40 +0100

    Make ruebe prefab changes.

--- Chronos/Assets/Scripts/CrystalSystem.cs
+++ Chronos/Assets/Scripts/CrystalSystem.cs
@@ -7,9 +7,10 @@ public class CrystalSystem : MonoBehaviour {
     public GameObject crystal;
     GameObject player;
     public Vector3 planetCenter;
+    float spawnCD = 0;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
@@ -22,52 +23,11 @@ public class CrystalSystem : MonoBehaviour {
     {
         if (crystalCount > 0)
         {
+            spawnCD = 0.3f;
             crystalCount -= 1;
-
-            //
-            Vector3 aiPos = this.transform.position;
-            Vector3 aiPlayerDir = (player.transform.position - this.transform.position).normalized;
-            Vector3 pointTowardsPlayer = aiPos + aiPlayerDir * 4;
-            //     Debug.DrawLine(aiPos, pointTowardsPlayer, Color.red, 1);
-
-            //Raycast experiment
-            RaycastHit hit;
-            //Vector3 planetPos = planet.transform.position;
-            // dir = (end - start).normalized
-            //  Vector3 dir = (planetCenter - player.transform.position).normalized;
-            //   Vector3 outsidePosition = planetCenter + dir * 50;
-            //   Vector3 outsideDirection = (planetCenter - outsidePosition).normalized;
-            //   Ray ray = new Ray(outsidePosition, outsideDirection);
-
-            //   Vector3 pointDirection = (pointTowardsPlayer - aiPos).normalized;
-
-
-            // Debug.DrawLine(outsidePosition, planetPos, Color.blue, Mathf.Infinity);
-
-            /*
-            //RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
-            {
-                if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Ground"))
-                {
-                    Vector3 dir = (planetCenter - player.transform.position).normalized;
-                    Vector3 spawnPos = player.transform.position + dir * (player.transform.localScale.x * 0.5f);
-
-
-                    GameObject createdDummy = GameObject.Instantiate(crystal, spawnPos, player.transform.rotation) as GameObject;
-            
-                }
-            }
-            //
-            */
-
-            //GameObject createdDummy = GameObject.Instantiate(crystal, player.transform.position, player.transform.rotation) as GameObject;
-            //Destroy(createdDummy, 3);
             Vector3 dir = (planetCenter - player.transform.position).normalized;
             Vector3 spawnPos = player.transform.position + dir * (player.transform.localScale.x * 0.5f);
-
             GameObject createdDummy = GameObject.Instantiate(crystal, spawnPos, player.transform.rotation) as GameObject;
-
         }
     }
 	
@@ -76,6 +36,11 @@ public class CrystalSystem : MonoBehaviour {
         if (Input.GetButtonDown("Action"))
         {
             UseCrystal();
+        }
+
+        if (spawnCD > 0)
+        {
+            spawnCD -= Time.deltaTime;
         }
     }
 }
